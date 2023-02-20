f57bba47985ffbb50afc671e4be5fb5069952719
shubham <shubham.bhoyar077@gmail.com>
2023-02-20 15:29:39 +0530

    Updated index.js file with time and nav module

--- js/index.js
+++ js/index.js
@@ -1,19 +1,15 @@
-import {Book} from "../modules/book.js"
+import {Book} from "../modules/book.js";
+import { showDateTime } from "../modules/showTime.js";
+import { navBar } from "../modules/navBar.js";
 
 const bookList = document.querySelector('.books');
 const bookName = document.getElementById('book_name');
 const autName = document.getElementById('auther_name');
 const addBtn = document.getElementById('addButton');
 
-
 const newBook = new Book(bookList);
+newBook.restoreBook();
 
-if (!localStorage.getItem('book_list')) {
-  newBook.updateLocalStorage();
-  newBook.restoreLocalStorage();
-} else {
-  newBook.restoreLocalStorage();
-}
 
 addBtn.addEventListener('click', () => {
   newBook.addBook(bookName.value, autName.value);
@@ -27,25 +23,9 @@ bookList.addEventListener('click', (e) => {
   }
 });
 
-
-// NavBar js
 const navList = document.querySelector('.nav-list');
-const bookSection = document.getElementById('book_section');
-const formSection = document.getElementById('form-section');
-const contactSection = document.getElementById('contact-section');
-
 navList.addEventListener('click', (event) => {
-  if (event.target.id === 'book-list') {
-    bookSection.classList.remove('disable-section');
-    formSection.classList.add('disable-section');
-    contactSection.classList.add('disable-section');
-  } else if (event.target.id === 'add-book') {
-    bookSection.classList.add('disable-section');
-    formSection.classList.remove('disable-section');
-    contactSection.classList.add('disable-section');
-  } else if (event.target.id === 'contact') {
-    bookSection.classList.add('disable-section');
-    formSection.classList.add('disable-section');
-    contactSection.classList.remove('disable-section');
-  }
+  navBar(event);
 });
+
+showDateTime();
