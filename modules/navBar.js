57ee97e4bb5981e3d8e71b92f1494ac63c5460ae
shubham <shubham.bhoyar077@gmail.com>
2023-02-20 15:44:39 +0530

    removed msg

--- modules/navBar.js
+++ modules/navBar.js
@@ -2,8 +2,10 @@
 const bookSection = document.getElementById('book_section');
 const formSection = document.getElementById('form-section');
 const contactSection = document.getElementById('contact-section');
+const bookAddMsg = document.getElementById('book-add-msg');
 
 export const navBar = (event) => {
+  bookAddMsg.classList.add('diable-msg');
   if (event.target.id === 'book-list') {
     bookSection.classList.remove('disable-section');
     formSection.classList.add('disable-section');
