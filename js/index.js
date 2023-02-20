6d4660006737828b8e83507fc3e733cc7aaf402c
shubham <shubham.bhoyar077@gmail.com>
2023-02-20 14:00:21 +0530

    Deleted book clss

--- js/index.js
+++ js/index.js
@@ -1,75 +1,12 @@
+import {Book} from "../modules/book.js"
+
 const bookList = document.querySelector('.books');
 const bookName = document.getElementById('book_name');
 const autName = document.getElementById('auther_name');
 const addBtn = document.getElementById('addButton');
 
-class Book {
-  constructor() {
-    this.bookStore = [
-      {
-        id: 'book_0',
-        title: 'object-oriented analysis and design',
-        author: 'Martin B',
-      },
-      {
-        id: 'book_1',
-        title: 'software design principles',
-        author: 'Jose K',
-      },
-    ];
-    this.bookCount = this.bookStore.length - 1;
 
-    this.displayBook = (book) => {
-      bookList.innerHTML += `
-          <li id="${book.id}">
-              <div class="content">
-                <span class="name">"${book.title}" by</span>
-                <span class="auther">${book.author}</span>
-              </div>
-              <button type="button" class="delete">Remove</button>
-          </li>
-      `;
-    };
-  }
-
-  addBook() {
-    this.bookCount += 1;
-    const objBook = {
-      id: `book_${this.bookCount}`,
-      title: bookName.value,
-      author: autName.value,
-    };
-
-    this.bookStore.push(objBook);
-    this.displayBook(objBook);
-    this.updateLocalStorage();
-
-    bookName.value = '';
-    autName.value = '';
-  }
-
-  removeBook(e) {
-    const parentli = e.target.parentElement;
-    this.bookStore = this.bookStore.filter((book) => book.id !== parentli.id);
-    bookList.removeChild(e.target.parentElement);
-    this.updateLocalStorage();
-  }
-
-  restoreLocalStorage() {
-    this.bookStore = JSON.parse(localStorage.getItem('book_list'));
-    this.bookCount = JSON.parse(localStorage.getItem('book_count'));
-    for (let i = 0; i < this.bookStore.length; i += 1) {
-      this.displayBook(this.bookStore[i]);
-    }
-  }
-
-  updateLocalStorage() {
-    localStorage.setItem('book_list', JSON.stringify(this.bookStore));
-    localStorage.setItem('book_count', JSON.stringify(this.bookCount));
-  }
-}
-
-const newBook = new Book();
+const newBook = new Book(bookList);
 
 if (!localStorage.getItem('book_list')) {
   newBook.updateLocalStorage();
@@ -79,7 +16,9 @@ if (!localStorage.getItem('book_list')) {
 }
 
 addBtn.addEventListener('click', () => {
-  newBook.addBook();
+  newBook.addBook(bookName.value, autName.value);
+  bookName.value = '';
+  autName.value = '';
 });
 
 bookList.addEventListener('click', (e) => {
@@ -88,6 +27,8 @@ bookList.addEventListener('click', (e) => {
   }
 });
 
+
+// NavBar js
 const navList = document.querySelector('.nav-list');
 const bookSection = document.getElementById('book_section');
 const formSection = document.getElementById('form-section');
